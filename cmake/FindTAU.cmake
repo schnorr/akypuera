8d399de2c46b88b583a3bf468c3a0aa9ae5127f7
Lucas Schnorr <Lucas.Schnorr@imag.fr>
2010-10-21 17:14:13 +0200

    updating FindTAU

--- cmake/FindTAU.cmake
+++ cmake/FindTAU.cmake
@@ -2,45 +2,55 @@
 # Find the TAU libraries and include dir
 #
 
-# TAU_INCLUDE_DIR  - Directories to include to use TAU
-# TAU_LIBRARY    - Files to link against to use TAU
+# TAU_INCLUDE_PATH  - Directories to include to use TAU
+# TAU_LIBRARY_PATH    - Files to link against to use TAU
 # TAU_FOUND        - When false, don't try to use TAU
 #
-# TAU_DIR can be used to make it simpler to find the various include
+# TAU_PATH can be used to make it simpler to find the various include
 # directories and compiled libraries when TAU was not installed in the
 # usual/well-known directories (e.g. because you made an in tree-source
 # compilation or because you installed it in an "unusual" directory).
-# Just set TAU_DIR it to your specific installation directory
+# Just set TAU_PATH it to your specific installation directory
 #
-FIND_LIBRARY(TAU_LIBRARY TAU_traceinput
-  PATHS
-  /usr/lib
-  /usr/local/lib
-  ${TAU_DIR}/lib
-)
+FIND_LIBRARY(TAU_LIBRARY
+  NAMES TAU
+  PATHS /usr/lib /usr/local/lib ${TAU_PATH}/lib ${TAU_PATH}/x86_64/lib/)
 
 IF(TAU_LIBRARY)
-  GET_FILENAME_COMPONENT(TAU_GUESSED_INCLUDE_DIR_tmp "${TAU_LIBRARY}" PATH)
-  STRING(REGEX REPLACE "lib$" "include" TAU_GUESSED_INCLUDE_DIR "${TAU_GUESSED_INCLUDE_DIR_tmp}")
+  MESSAGE ( STATUS "Found TAU: ${TAU_LIBRARY}" )
+  GET_FILENAME_COMPONENT(TAU_LIBRARY_tmp "${TAU_LIBRARY}" PATH)
+  SET (TAU_LIBRARY_PATH CACHE PATH "" ${TAU_LIBRARY_tmp})
+ELSE(TAU_LIBRARY)
+  SET (TAU_LIBRARY_PATH CACHE PATH "" "TAU_LIBRARY_PATH-NOTFOUND")
+  unset(LIBRARY_PATH CACHE)
 ENDIF(TAU_LIBRARY)
 
-FIND_PATH( TAU_INCLUDE_DIR TAU_tf.h
+FIND_PATH( TAU_INCLUDE_tmp TAU_tf.h
   PATHS
-  ${TAU_GUESSED_INCLUDE_DIR}
-  ${TAU_DIR}/include
+  ${TAU_GUESSED_INCLUDE_PATH}
+  ${TAU_PATH}/include
   /usr/include
   /usr/local/include
 )
 
+IF(TAU_INCLUDE_tmp)
+  SET (TAU_INCLUDE_PATH "${TAU_INCLUDE_tmp}")
+ELSE(TAU_INCLUDE_tmp)
+  SET (TAU_INCLUDE_PATH CACHE PATH "" "TAU_INCLUDE_PATH-NOTFOUND")
+ENDIF(TAU_INCLUDE_tmp)
+
+IF( TAU_INCLUDE_PATH )
+  IF( TAU_LIBRARY_PATH )
+    SET( TAU_FOUND TRUE )
+  ENDIF ( TAU_LIBRARY_PATH )
+ENDIF( TAU_INCLUDE_PATH )
+
 IF( NOT TAU_FOUND )
-  MESSAGE("TAU installation was not found. Please provide TAU_DIR:")
-  MESSAGE("  - through the GUI when working with ccmake, ")
-  MESSAGE("  - as a command line argument when working with cmake e.g. ")
-  MESSAGE("    cmake .. -DTAU_DIR:PATH=/usr/local/tau ")
-  MESSAGE("Note: the following message is triggered by cmake on the first ")
-  MESSAGE("    undefined necessary PATH variable (e.g. TAU_INCLUDE_DIR).")
-  MESSAGE("    Providing TAU_DIR (as above described) is probably the")
-  MESSAGE("    simplest solution unless you have a really customized/odd")
-  MESSAGE("    TAU installation...")
-  SET(TAU_DIR "" CACHE PATH "Root of TAU install tree." )
+  MESSAGE(STATUS "TAU installation was not found. Please provide TAU_PATH:")
+  MESSAGE(STATUS "  - through the GUI when working with ccmake, ")
+  MESSAGE(STATUS "  - as a command line argument when working with cmake e.g.")
+  MESSAGE(STATUS "    cmake .. -DTAU_PATH:PATH=/usr/local/tau ")
+  SET(TAU_PATH "" CACHE PATH "Root of TAU install tree." )
 ENDIF( NOT TAU_FOUND )
+
+unset(TAU_INCLUDE_tmp)
