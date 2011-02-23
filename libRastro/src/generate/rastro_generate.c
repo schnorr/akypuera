dc81b5dd8e8a87734c0f89bb7b7288361d6dcf97
Rodrigo Kassick <kassick@gmail.com>
2011-02-23 15:14:20 +0100

    rastro_generate.c: Added getopt support: -c <cfile>  -h <headerfile> -f <fortranfunctionsfile>
    Added support for fortran-compatible c-functions (all pointer parameters,
    void return, ending with a _ ). Just wrap around the
    existing standard-c counterparts

--- libRastro/src/generate/rastro_generate.c
+++ libRastro/src/generate/rastro_generate.c
@@ -26,58 +26,75 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <getopt.h>
 
 int validate_argtypes(char *linha)
 {
     return strspn(linha, XSTR(LETRA_DOUBLE) XSTR(LETRA_UINT64) XSTR(LETRA_FLOAT) XSTR(LETRA_UINT32) XSTR(LETRA_UINT16) XSTR(LETRA_UINT8) XSTR(LETRA_STRING)) == strlen(linha);
 }
 
-void break_types(char *argtypes, char *arglist, char *varlist, counters_t *ct)
+void break_types(char *argtypes, char *arglist, char *varlist, char * arglist_f, char * params_f, counters_t *ct)
 {
     char *s = argtypes;
     char c;
-    int na, nv;
+    int na, nv, nf, np;
     
     ct->n_uint16 = ct->n_uint8 = ct->n_uint64 = ct->n_uint32 = 0;
     ct->n_float = ct->n_double = ct->n_string = 0;
 
-    na = sprintf(arglist, "u_int16_t type");
+    na = sprintf(arglist,   "u_int16_t type");
+    nf = sprintf(arglist_f, "u_int16_t *type");
+    np = sprintf(params_f,  "*type");
     nv = sprintf(varlist, "type");
     while ((c = *s++) != '\0') {
 	switch (c) {
 	case LETRA_UINT32_ASPA:
-	    na += sprintf(arglist+na, ", u_int32_t " XSTR(LETRA_UINT32) "%d", ct->n_uint32);
-	    nv += sprintf(varlist+nv, ", " XSTR(LETRA_UINT32) "%d", ct->n_uint32);
+	    na += sprintf(arglist+na,   ", u_int32_t "  XSTR(LETRA_UINT32) "%d",  ct->n_uint32);
+	    nf += sprintf(arglist_f+nf, ", u_int32_t *" XSTR(LETRA_UINT32) "%d",  ct->n_uint32);
+            np += sprintf(params_f +np, ", *"           XSTR(LETRA_UINT32) "%d",  ct->n_uint32);
+	    nv += sprintf(varlist+nv,   ", "            XSTR(LETRA_UINT32) "%d",  ct->n_uint32);
             ct->n_uint32++;
 	    break;
 	case LETRA_UINT16_ASPA:
-	    na += sprintf(arglist+na, ", u_int16_t " XSTR(LETRA_UINT16) "%d", ct->n_uint16);
-	    nv += sprintf(varlist+nv, ", " XSTR(LETRA_UINT16) "%d", ct->n_uint16);
+	    na += sprintf(arglist+na,    ", u_int16_t "  XSTR(LETRA_UINT16) "%d", ct->n_uint16);
+	    nf += sprintf(arglist_f+nf,  ", u_int16_t *" XSTR(LETRA_UINT16) "%d", ct->n_uint16);
+            np += sprintf(params_f +np,  ", *"           XSTR(LETRA_UINT16) "%d", ct->n_uint16);
+	    nv += sprintf(varlist+nv,    ", "            XSTR(LETRA_UINT16) "%d", ct->n_uint16);
 	    ct->n_uint16++;
 	    break;
 	case LETRA_UINT8_ASPA:
-	    na += sprintf(arglist+na, ", u_int8_t " XSTR(LETRA_UINT8) "%d", ct->n_uint8);
-	    nv += sprintf(varlist+nv, ", " XSTR(LETRA_UINT8) "%d", ct->n_uint8);
+	    na += sprintf(arglist+na,    ", u_int8_t "  XSTR(LETRA_UINT8) "%d",   ct->n_uint8);
+	    nf += sprintf(arglist_f+nf,  ", u_int8_t *" XSTR(LETRA_UINT8) "%d",   ct->n_uint8);
+            np += sprintf(params_f +np,  ", *"          XSTR(LETRA_UINT8) "%d",   ct->n_uint8);
+	    nv += sprintf(varlist+nv,    ", "           XSTR(LETRA_UINT8) "%d",   ct->n_uint8);
             ct->n_uint8++;
 	    break;
 	case LETRA_FLOAT_ASPA:
-	    na += sprintf(arglist+na, ", float " XSTR(LETRA_FLOAT) "%d", ct->n_float);
-	    nv += sprintf(varlist+nv, ", " XSTR(LETRA_FLOAT) "%d", ct->n_float);
+	    na += sprintf(arglist+na,    ", float "  XSTR(LETRA_FLOAT) "%d",      ct->n_float);
+	    nf += sprintf(arglist_f+nf,  ", float *" XSTR(LETRA_FLOAT) "%d",      ct->n_float);
+            np += sprintf(params_f +np,  ", *"       XSTR(LETRA_FLOAT) "%d",      ct->n_float);
+	    nv += sprintf(varlist+nv,    ", "        XSTR(LETRA_FLOAT) "%d",      ct->n_float);
             ct->n_float++;
 	    break;
 	case LETRA_DOUBLE_ASPA:
-	    na += sprintf(arglist+na, ", double " XSTR(LETRA_DOUBLE) "%d", ct->n_double);
-	    nv += sprintf(varlist+nv, ", " XSTR(LETRA_DOUBLE) "%d", ct->n_double);
+	    na += sprintf(arglist+na,    ", double "  XSTR(LETRA_DOUBLE) "%d",    ct->n_double);
+	    nf += sprintf(arglist_f+nf,  ", double *" XSTR(LETRA_DOUBLE) "%d",    ct->n_double);
+              np += sprintf(params_f +np,  ", *"      XSTR(LETRA_DOUBLE) "%d",    ct->n_double);
+	    nv += sprintf(varlist+nv,    ", "         XSTR(LETRA_DOUBLE) "%d",    ct->n_double);
             ct->n_double++;
 	    break;
 	case LETRA_UINT64_ASPA:
-	    na += sprintf(arglist+na, ", u_int64_t " XSTR(LETRA_UINT64) "%d", ct->n_uint64);
-	    nv += sprintf(varlist+nv, ", " XSTR(LETRA_UINT64) "%d", ct->n_uint64);
+	    na += sprintf(arglist+na,    ", u_int64_t "  XSTR(LETRA_UINT64) "%d", ct->n_uint64);
+	    nf += sprintf(arglist_f+nf,  ", u_int64_t *" XSTR(LETRA_UINT64) "%d", ct->n_uint64);
+            np += sprintf(params_f +np,  ", *"           XSTR(LETRA_UINT64) "%d", ct->n_uint64);
+	    nv += sprintf(varlist+nv,    ", "            XSTR(LETRA_UINT64) "%d", ct->n_uint64);
             ct->n_uint64++;
 	    break;
 	case LETRA_STRING_ASPA:
-	    na += sprintf(arglist+na, ", char *" XSTR(LETRA_STRING) "%d", ct->n_string);
-	    nv += sprintf(varlist+nv, ", " XSTR(LETRA_STRING) "%d", ct->n_string);
+	    na += sprintf(arglist+na,    ", char *" XSTR(LETRA_STRING) "%d",      ct->n_string);
+	    nf += sprintf(arglist_f+nf,  ", char *" XSTR(LETRA_STRING) "%d",      ct->n_string);
+            np += sprintf(params_f +np,  ", "       XSTR(LETRA_STRING) "%d",      ct->n_string);
+	    nv += sprintf(varlist+nv,    ", "       XSTR(LETRA_STRING) "%d",      ct->n_string);
             ct->n_string++;
 	    break;
 	}
@@ -177,14 +194,30 @@ void generate_body(char *argtypes, char *arglist, counters_t *ct, FILE *cfile)
     fprintf(cfile, "}\n\n");
 }
 
-void generate_function(char *argtypes, FILE *hfile, FILE *cfile)
+void generate_body_f(char *argtypes, char *arglist, char *params_f, counters_t *ct, FILE *cfile)
 {
-    char arglist[1000], varlist[1000];
+    int i;
+    
+    fprintf(cfile, "void rst_event_%s_ptr_f_(rst_buffer_t *ptr, %s)\n",
+                        argtypes, arglist);
+    fprintf(cfile, "{\n");
+
+    fprintf(cfile, "\t" "rst_event_%s_ptr( ptr, %s);"  "\n", argtypes, params_f);
+    fprintf(cfile, "}\n");
+}
+
+void generate_function(char *argtypes, FILE *hfile, FILE *cfile, FILE *ffile)
+{
+    char arglist[1000], varlist[1000],arglist_f[1000], params_f[1000];
     counters_t ct;
     
-    break_types(argtypes, arglist, varlist, &ct);
-    generate_header(argtypes, arglist, varlist, hfile);
-    generate_body(argtypes, arglist, &ct, cfile);
+    break_types(argtypes, arglist, varlist, arglist_f, params_f, &ct);
+    if (hfile)
+      generate_header(argtypes, arglist, varlist, hfile);
+    if(cfile)
+      generate_body(argtypes, arglist, &ct, cfile);
+    if(ffile)
+      generate_body_f(argtypes,arglist_f, params_f, &ct,ffile);
 }
 
 void generate_hfile_start(FILE *hfile, char *hfilename)
@@ -217,6 +250,13 @@ void generate_cfile_start(FILE *cfile, char *hfilename)
     fprintf(cfile, "#include \"%s\"\n\n", hfilename);
 }
 
+void generate_ffile_start(FILE *ffile, char *hfilename)
+{
+    fprintf(ffile,
+	    "/* Do not edit. File generated by rastro_generate. */\n\n");
+    fprintf(ffile, "#include \"%s\"\n\n", hfilename);
+}
+
 FILE *openfile(char *name, char *mode)
 {
     FILE *f;
@@ -237,32 +277,77 @@ FILE *openfile(char *name, char *mode)
 */
 int main(int argc, char **argv)
 {
-    char *hfilename;
-    char *cfilename;
+    char *hfilename = NULL;
+    char *cfilename = NULL;
+    char *ffilename = NULL;
     FILE *entrada;
-    FILE *hfile;
-    FILE *cfile;
-    char argtypes[1000];
+    FILE *hfile = NULL;
+    FILE *cfile = NULL;
+    FILE *ffile = NULL;
+    char argtypes[1000], opt;
 
-    if ( argc != 4 ) {
+    while ((opt = getopt(argc,argv,"c:h:f:")) != -1 ) {
+      switch (opt) {
+        case 'f':
+          ffilename = strdup(optarg);
+          break;
+        case 'c':
+          cfilename = strdup(optarg);
+          break;
+        case 'h':
+          hfilename = strdup(optarg);
+          break;
+        default:
+          printf("opt is %d\n",opt);
+          printf("Usage: %s -h file.h -c file.c -f file_f.c inputfile\
+              where file.c and file.h are header and functions for C programs\
+              file_f.c contains functions for FORTRAN applications, needs file.c and file.c\n", argv[0]);
+          exit(0);
+          break;
+      }
+    }
+
+    if (optind >= argc) {
+      printf("Missing input file\n");
+      return 1;
+    }
+
+
+
+
+    /* Ugly.
+     if ( argc != 4 ) {
         printf("rastro_generate functions name.c name.h\n");
         exit(0);
-    }
+    } 
     cfilename = argv[2];
     hfilename = argv[3];
-    entrada = openfile(argv[1], "r");
-    cfile = openfile(cfilename, "w");
-    hfile = openfile(hfilename, "w");
+    */
+    
+    entrada = openfile(argv[optind], "r");
 
-    generate_hfile_start(hfile, hfilename);
-    generate_cfile_start(cfile, hfilename);
+    // Only generates file names as required
+    if (cfilename)
+      cfile = openfile(cfilename, "w");
+    if (hfilename)
+      hfile = openfile(hfilename, "w");
+    if (ffilename)
+      ffile = openfile(ffilename, "w");
+
+    // Make headers
+    if (hfilename)
+      generate_hfile_start(hfile, hfilename);
+    if (cfilename)
+      generate_cfile_start(cfile, hfilename);
+    if (ffilename)
+      generate_ffile_start(ffile, hfilename);
     
     while (fscanf(entrada, "%s", argtypes) == 1) {
 	if (!validate_argtypes(argtypes)) {
 	    fprintf(stderr, "Invalid event types \"%s\" ignored.\n", argtypes);
             continue;
         }
-        generate_function(argtypes, hfile, cfile);
+        generate_function(argtypes, hfile, cfile, ffile);
     }
     
     generate_hfile_end(hfile);
