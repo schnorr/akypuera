94a2152c3bd6c5d4c1c436576586f3962b4e967b
Lucas Schnorr <Lucas.Schnorr@imag.fr>
2011-12-12 15:26:12 +0100

    otf2paje now converts states

--- src/otf2paje.c
+++ src/otf2paje.c
@@ -19,6 +19,19 @@
 #include <string.h>
 #include <argp.h>
 #include <otf2/otf2.h>
+#include <scorep_utility/SCOREP_Hashtab.h>
+#include <scorep_utility/SCOREP_Vector.h>
+#include "aky_private.h"
+
+/* time_to_seconds */
+static double time_to_seconds(double time)
+{
+  static double first_time = -1;
+  if (first_time == -1){
+    first_time = time;
+  }
+  return (time - first_time) / 1000000;
+}
 
 /* Parameter handling */
 static char doc[] = "Converts an OTF2 archive to the Paje file format";
@@ -70,16 +83,103 @@ static int parse_options (int key, char *arg, struct argp_state *state)
 
 static struct argp argp = { options, parse_options, args_doc, doc };
 
+/* Data utilities for convertion */
+struct otf2paje_s
+{
+    OTF2_Reader*    reader;
+    SCOREP_Vector*  locations;
+    SCOREP_Hashtab* regions;
+    SCOREP_Hashtab* strings;
+    SCOREP_Hashtab* groups;
+};
+typedef struct otf2paje_s otf2paje_t;
+
+struct otf2paje_region_s
+{
+    uint32_t region_id;
+    uint32_t string_id;
+};
+typedef struct otf2paje_region_s otf2paje_region_t;
+
+struct otf2paje_string_s
+{
+    uint32_t string_id;
+    char*    content;
+};
+typedef struct otf2paje_string_s otf2paje_string_t;
+
+/* Definition callbacks */
+static SCOREP_Error_Code GlobDefString_print (void* userData,
+                                              uint32_t stringID,
+                                              char* string)
+{
+  otf2paje_t* data = (otf2paje_t*) userData;
+  size_t hint;
+  SCOREP_Hashtab_Entry* entry;
+  entry = SCOREP_Hashtab_Find (data->strings, &stringID, &hint);
+  if (entry) {
+    /* already defined? */
+    return SCOREP_SUCCESS;
+  }
+
+  otf2paje_string_t* new_string;
+  new_string = (otf2paje_string_t*) malloc (sizeof (otf2paje_string_t));
+  new_string->string_id = stringID;
+  new_string->content = (char*) malloc (strlen(string)+1);
+  strcpy (new_string->content, string);
+  SCOREP_Hashtab_Insert (data->strings,
+                         &new_string->string_id,
+                         new_string,
+                         &hint);
+  return SCOREP_SUCCESS;
+}
+
+static SCOREP_Error_Code GlobDefRegion_print (void* userData,
+                                              uint32_t regionID,
+                                              uint32_t stringID,
+                                              uint32_t description,
+                                              OTF2_GlobRegionType regionType,
+                                              uint32_t sourceFile,
+                                              uint32_t beginLineNumber,
+                                              uint32_t endLineNumber)
+{
+  otf2paje_t* data = (otf2paje_t*) userData;
+  size_t hint;
+  SCOREP_Hashtab_Entry* entry;
+  entry = SCOREP_Hashtab_Find (data->regions, &regionID, &hint);
+  if (entry) {
+    /* already defined? */
+    return SCOREP_SUCCESS;
+  }
+  otf2paje_region_t* new_region = (otf2paje_region_t*) malloc (sizeof(otf2paje_region_t));
+  new_region->region_id = regionID;
+  new_region->string_id = stringID;
+  SCOREP_Hashtab_Insert( data->regions,
+                         &new_region->region_id,
+                         new_region,
+                         &hint );
+  return SCOREP_SUCCESS;
+}
+
+/* Events callbacks */
 static SCOREP_Error_Code Enter_print (uint64_t locationID,
                                       uint64_t time,
                                       void *userData,
                                       OTF2_AttributeList* attributes,
                                       uint32_t regionID)
 {
-  printf ("ENTER %lu %lu %p %p %d\n", locationID, time, userData, attributes, regionID);
+  otf2paje_t* data = (otf2paje_t*) userData;
+  SCOREP_Hashtab_Entry *e1 = SCOREP_Hashtab_Find (data->regions, &regionID, NULL);
+  otf2paje_region_t *region = (otf2paje_region_t*) e1->value;
+  SCOREP_Hashtab_Entry *e2 = SCOREP_Hashtab_Find (data->strings, &region->string_id,NULL);
+  otf2paje_string_t *string = (otf2paje_string_t*) e2->value;
+  char *state_name = string->content;
+
+  char mpi_process[100];
+  snprintf(mpi_process, 100, "rank%lu", locationID);
+  pajePushState(time_to_seconds(time), mpi_process, "STATE", state_name);
   return SCOREP_SUCCESS;
 }
-
 
 static SCOREP_Error_Code Leave_print (uint64_t locationID,
                                       uint64_t time,
@@ -87,7 +187,9 @@ static SCOREP_Error_Code Leave_print (uint64_t locationID,
                                       OTF2_AttributeList* attributes,
                                       uint32_t regionID)
 {
-  printf ("LEAVE %lu %lu %p %p %d\n", locationID, time, userData, attributes, regionID);
+  char mpi_process[100];
+  snprintf(mpi_process, 100, "rank%lu", locationID);
+  pajePopState(time_to_seconds(time), mpi_process, "STATE");
   return SCOREP_SUCCESS;
 }
 
@@ -112,6 +214,56 @@ int main (int argc, char **argv)
     return 1;
   }
 
+  /* User data for callbacks. */
+  otf2paje_t *user_data = (otf2paje_t*) malloc (sizeof (otf2paje_t));
+  user_data->reader    = reader;
+  user_data->locations = SCOREP_Vector_Create();
+  user_data->regions   = SCOREP_Hashtab_CreateSize( 128,
+                                                    SCOREP_Hashtab_HashInt32,
+                                                    SCOREP_Hashtab_CompareUint32 );
+  user_data->strings = SCOREP_Hashtab_CreateSize( 256,
+                                                  SCOREP_Hashtab_HashInt32,
+                                                  SCOREP_Hashtab_CompareUint32 );
+  user_data->groups = SCOREP_Hashtab_CreateSize( 128,
+                                                 SCOREP_Hashtab_HashInt64,
+                                                 SCOREP_Hashtab_CompareUint64 );
+
+  /* Define definition callbacks. */
+  OTF2_GlobDefReaderCallbacks register_defs;
+  bzero(&register_defs, sizeof (OTF2_GlobDefReaderCallbacks));
+  register_defs.GlobDefString         = GlobDefString_print;
+  /* register_defs.GlobDefLocation       = GlobDefLocation_print; */
+  register_defs.GlobDefRegion         = GlobDefRegion_print;
+  /* register_defs.GlobDefGroup          = GlobDefGroup_print; */
+  /* register_defs.GlobDefSystemTreeNode = GlobDefSystemTreeNode_print; */
+  /* register_defs.GlobDefLocationGroup  = GlobDefLocationGroup_print; */
+
+  /* Read global definitions. */
+  OTF2_GlobDefReader* glob_def_reader  = OTF2_Reader_GetGlobDefReader (reader);
+  OTF2_Reader_RegisterGlobDefCallbacks (reader, glob_def_reader,
+                                        &register_defs,
+                                        sizeof (register_defs),
+                                        user_data);
+  uint64_t definitions_read = 0;
+  OTF2_Reader_ReadGlobalDefinitions (reader, glob_def_reader,
+                                     OTF2_UNDEFINED_UINT64,
+                                     &definitions_read);
+  /* Check if number of global definitions read matches the number of global
+   * definitions defined in the anchor file. */
+  uint64_t defs_anchor = 0;
+  OTF2_Reader_GetNumberOfGlobalDefinitions (reader, &defs_anchor);
+  if (definitions_read != defs_anchor){
+    fprintf(stderr,
+            "[otf2paje] at %s, "
+            "Number of global definitions read and "
+            "defined in anchor file do not match.\n",
+            __FUNCTION__);   
+    return 1;
+  }
+
+  /* we start here to output the paje converted file */
+  paje_header();
+  paje_hierarchy();
 
   /* Get number of locations from the anchor file. */
   uint64_t          num_locations = 0;
@@ -122,6 +274,10 @@ int main (int argc, char **argv)
     uint64_t definitions_read = 0;
     OTF2_Reader_ReadAllLocalDefinitions (reader, def_reader, &definitions_read);
     OTF2_Reader_CloseDefReader (reader, def_reader);
+
+    char mpi_process[100];
+    snprintf(mpi_process, 100, "rank%lu", i);
+    pajeCreateContainer(0, mpi_process, "PROCESS", "0", mpi_process);
   }
 
   /* Define event callbacks. */
@@ -138,7 +294,8 @@ int main (int argc, char **argv)
                                         glob_evt_reader,
                                         &print,
                                         sizeof (print),
-                                        NULL);
+                                        user_data);
+
 
   /* Read until events are all read. */
   uint64_t events_read = UINT64_MAX;
@@ -147,6 +304,12 @@ int main (int argc, char **argv)
                                                              glob_evt_reader,
                                                              UINT64_MAX,
                                                              &events_read );
+  }
+
+  for (size_t i = 0; i < num_locations; i++){
+    char mpi_process[100];
+    snprintf(mpi_process, 100, "rank%lu", i);
+    /* pajeDestroyContainer(0, "PROCESS", mpi_process); */
   }
 
   OTF2_Reader_Delete (reader);
