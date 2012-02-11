81156f0bd7f0a9b7a36e088f1acd3687b81e95c8
Lucas Schnorr <Lucas.Schnorr@imag.fr>
2012-02-11 09:03:57 +0100

    sleep for 1 second between init and finalize

--- examples/prog.c
+++ examples/prog.c
@@ -1,10 +1,12 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char** argv) {
 
    MPI_Init(&argc, &argv);
+   sleep(1);
    MPI_Finalize();
    return 0;
 }
